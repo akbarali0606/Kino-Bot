4d822716a6bc58aa3511721b4090932777f4c04a
akbarali0606 <tursunboyevakbarali807@gmail.com>
2025-04-15 21:17:39 +0500

    Update kino.py

--- KINO/kino.py
+++ KINO/kino.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 
-TOKEN = "7991412037:AAE3lXzplwNiGoIjnPXyeop3LUoQWCyVBuk"
+TOKEN = "7991412037:AAHaFKmWk1UzdHLBobw5DUYq94UJD0v4S5k"
 ADMIN_ID = 907402803
 MOVIE_FILE = "movies.json"
 USER_FILE = "users.json"
